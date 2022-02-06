ad60219a4fcbbc5d919ed47d1cfe147bbff0a53b
mrCodeAI <david_bystrom@live.se>
2022-02-06 15:37:21 +0100

    Fixed Cookie Button

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,4 @@ function btnClose() {
     document.getElementById('cookie-banner').style.display="none";
 }
 
-cookieBtn.addEventListener("click", btnClose());
+cookieBtn.addEventListener("click", btnClose);
